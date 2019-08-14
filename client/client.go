8855277a1dfe1f970968656d675f0a741d9f0ff5
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-14 18:40:33 +0200

    renamed package

--- client/client.go
+++ client/client.go
@@ -1,6 +1,6 @@
 // Client is a dbmon CLI client connecting to the server to pull data
 // It then can display, store, or relay it to a monitoring tool.
-package client
+package dbmonclient
 
 type client struct {
 	endpoint string // Endpoint domain or IP to interrogate
--- client/request.go
+++ client/request.go
@@ -1,4 +1,4 @@
-package client
+package dbmonclient
 
 type Request struct {
 	cluster string // Target cluster identification
