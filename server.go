ab3dd066a4feab26adf4b84559218d8bedd1d683
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-14 18:42:04 +0200

    implemented basic server functions

--- server.go
+++ server.go
@@ -1,10 +1,20 @@
 // dbmon is a server daemon with a gRPC API endpoint that serves information about a database cluster
 package dbmon
 
+import (
+	"context"
+	pb "github.com/bytemare/dbmon/dbmon"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"net"
+)
+
 // DBMon
 type DBMon struct {
 	data   map[*Cluster]report // Cache to hold data to serve
 	source <-chan [2]string    // Channel through which data is arriving
+	port	string				// Network port to listen on
+	grpc	*grpc.Server		// The gRPC Server Handle
 }
 
 // report holds a cluster's accumulated information
@@ -14,16 +24,41 @@ type report struct {
 }
 
 // NewDBMon initialises and returns a new DBMon struct
-func NewDBMon() (*DBMon, error) {
-	return nil, nil
+func NewDBMon(port string) *DBMon {
+	return &DBMon{
+		data:   make(map[*Cluster]report),
+		source: make(chan [2]string),
+		port:port,
+		grpc: grpc.NewServer(),
+	}
 }
 
 // Start starts the DBMon server
-func (mon *DBMon) Start() error {
-	return nil
+func (mon *DBMon) Start() {
+
+	lis, err := net.Listen("tcp", mon.port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	pb.RegisterHealCheckServer(mon.grpc, mon)
+	if err := mon.grpc.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+
+	}
 }
 
 // Stop stops the DBMon server
-func (mon *DBMon) Stop() error {
-	return nil
+func (mon *DBMon) Stop() {
+	mon.grpc.GracefulStop()
+}
+
+/**
+** Implement the gRPC
+ */
+
+// SayHello implements HealthCheckerServer
+func (mon DBMon) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
