883e9180de6c6abdcb1dba14306b75704bde5714
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-15 04:56:32 +0200

    now serve real reports

--- server.go
+++ server.go
@@ -1,35 +1,40 @@
-// dbmon is a server daemon with a gRPC API endpoint that serves information about a database cluster
 package dbmon
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	pb "github.com/bytemare/dbmon/dbmon"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net"
+	"sync"
 )
 
 // DBMon
 type DBMon struct {
-	data   map[*Cluster]report // Cache to hold data to serve
-	source <-chan [2]string    // Channel through which data is arriving
-	port	string				// Network port to listen on
-	grpc	*grpc.Server		// The gRPC Server Handle
-}
-
-// report holds a cluster's accumulated information
-type report struct {
-	clusterID *Cluster
-	data      []string
+	// TODO this may not be very memrory efficient, look for a better solution
+	clusters map[string]*Cluster
+	data     map[string][]*pb.Report // Cache to hold data to serve per cluster : a list of reports
+	mux      sync.Mutex              // Mutex to protect against concurrent read/write on cache
+	source   <-chan *pb.Report       // Channel through which data is arriving
+	sync     chan struct{}           // Stop signal channel
+	port     string                  // Network port to listen on
+	grpc     *grpc.Server            // The gRPC Server Handle
 }
 
 // NewDBMon initialises and returns a new DBMon struct
-func NewDBMon(port string) *DBMon {
+func NewDBMon(port string, serverChan <-chan *pb.Report) *DBMon {
 	return &DBMon{
-		data:   make(map[*Cluster]report),
-		source: make(chan [2]string),
-		port:port,
-		grpc: grpc.NewServer(),
+		clusters: make(map[string]*Cluster),
+		data:     make(map[string][]*pb.Report),
+		mux:      sync.Mutex{},
+		source:   serverChan,
+		sync:     make(chan struct{}),
+		port:     port,
+		grpc:     grpc.NewServer(),
 	}
 }
 
@@ -44,12 +49,53 @@ func (mon *DBMon) Start() {
 	pb.RegisterHealCheckServer(mon.grpc, mon)
 	if err := mon.grpc.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-
 	}
+
+loop:
+	for {
+		select {
+
+		case <-mon.sync:
+			log.Info("Server shutting down.")
+			break loop
+
+		case r := <-mon.source:
+			log.Info("Server received a new report")
+			mon.mux.Lock()
+			mon.data[r.ClusterID] = append(mon.data[r.ClusterID], r)
+		}
+	}
+
+}
+
+// RegisterCluster adds a new cluster to serve reports for
+func (mon *DBMon) RegisterCluster(cluster *Cluster) error {
+	if _, ok := mon.clusters[cluster.id]; ok {
+		log.Errorf("Cluster '%s' has already been registered.", cluster.id)
+		return errors.New(fmt.Sprintf("Cluster registration failed : value already exists '%s'.", cluster.id))
+	} else {
+		mon.clusters[cluster.id] = cluster
+		mon.data[cluster.id] = []*pb.Report{}
+		return nil
+	}
+}
+
+// Returns the list of strings representing reports
+func (mon *DBMon) extractReports(clusterID string) []*pb.Report {
+
+	mon.mux.Lock()
+	reports := mon.data[clusterID]
+	output := make([]*pb.Report, len(reports))
+	copy(output, reports)
+	delete(mon.data, clusterID)
+	mon.mux.Unlock()
+
+	return output
 }
 
 // Stop stops the DBMon server
 func (mon *DBMon) Stop() {
+	mon.sync <- struct{}{}
 	mon.grpc.GracefulStop()
 }
 
@@ -58,7 +104,34 @@ func (mon *DBMon) Stop() {
  */
 
 // SayHello implements HealthCheckerServer
-func (mon DBMon) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+func (mon DBMon) Pull(ctx context.Context, in *pb.PullRequest) (*pb.PullReply, error) {
+	log.Info("Received pull for : %v", in.ClusterId)
+
+	reply := &pb.PullReply{
+		ClusterId:            in.ClusterId,
+		Reports:              nil,
+		Error:                nil,
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+
+	// Verify if cluster is registered
+	if _, ok := mon.clusters[in.ClusterId]; !ok {
+		/*err := &pb.Error{
+			Code:                 uint32(5),
+			Message:              "This cluster is not registered",
+			XXX_NoUnkeyedLiteral: struct{}{},
+			XXX_unrecognized:     nil,
+			XXX_sizecache:        0,
+		}
+		reply.Error = err*/
+		err1 := status.Error(codes.NotFound, "id was not found")
+		return nil, err1
+		//return reply, errors.New(err.Message)
+	}
+
+	reply.Reports = mon.extractReports(in.ClusterId)
+
+	return reply, nil
 }
