2ad61333abf3a37adda98e6e008887b97631c57d
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-16 01:31:49 +0200

    minor stuff

--- cluster.go
+++ cluster.go
@@ -1,9 +1,9 @@
-// Cluster represents a registered Database Cluster to a dbmon instance
 package dbmon
 
 import "time"
 
-// Cluster holds identity information about a registered Cluster
+// Cluster // Cluster represents a registered Database Cluster to a dbmon instance.
+// It holds identity information about a Cluster and a corresponding Connector.
 type Cluster struct {
 	id        string    // Identification/Name for the target cluster
 	connector Connector // Connector enabling communicating with the cluster
@@ -25,6 +25,7 @@ type requestInstance struct {
 	stat     time.Duration // Time the query has taken to operate
 }
 
+// NewCluster returns a new initialised Cluster struct
 func NewCluster(id string, c Connector) *Cluster {
 	return &Cluster{
 		id:        id,
