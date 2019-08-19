b9b39b6a3a26769fa7b42d57cbc1545d137204de
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-19 02:50:28 +0200

    Successful retrieval and analysis of cluster health, and display on client

--- Tests/client/dbmon-client.go
+++ Tests/client/dbmon-client.go
@@ -2,53 +2,301 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	cockroachdb "github.com/bytemare/dbmon/connectors/CockroachDB"
 	"github.com/bytemare/dbmon/dbmon"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 const addr = "localhost"
 const port = "4000"
 const clusterId = "roachy"
+const refresh  = 2 * time.Second
+const timeout = 10*time.Second
 
-func main() {
+func printNode(node *cockroachdb.NodeStats) {
+	log.Infof("Node Info : %d", node.ID)
+	log.Infof("Ranges: %d", node.Ranges)
+	log.Infof("Replicas : %d", node.Replicas)
+	log.Infof("Cap. Usage : %d", node.CapacityUsage)
+	log.Infof("Cap. Reserved : %d", node.CapacityReserved)
+	log.Infof("Cap. Available : %d", node.CapacityAvailable)
+	log.Infof("Cap. : %d", node.Capacity)
+	log.Infof("SystemMem : %d", node.TotalSystemMemory)
+	log.Infof("Q/s %f", node.QueriesPerSecond)
+	log.Infof("Heartbeat Latency : %d", node.Latency99)
+	log.Infof("ClockOffset : %d", node.ClockOffset)
+}
+
+func setupConnection() *Connection {
+
+	connection := &Connection{
+		Conn:   nil,
+		Client: nil,
+		Ctx:    nil,
+		Cancel: nil,
+	}
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(addr+":"+port, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
-	c := dbmon.NewHealCheckClient(conn)
 
-	// Contact the server and print out its response.
+	connection.Conn = conn
+	connection.Client = dbmon.NewHealCheckClient(connection.Conn)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	connection.Ctx = ctx
+	connection.Cancel = cancel
 
-	time.Sleep(7 * time.Second)
+	return connection
+}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.Pull(ctx, &dbmon.PullRequest{
+type Connection struct {
+	Conn   *grpc.ClientConn
+	Client dbmon.HealCheckClient
+	Ctx    context.Context
+	Cancel context.CancelFunc
+}
+
+func (c *Connection) close() {
+	_ = c.Conn.Close()
+	c.Cancel()
+}
+
+func (c *Connection) Pull() (*dbmon.PullReply, error) {
+
+	reply, err := c.Client.Pull(c.Ctx, &dbmon.PullRequest{
 		ClusterId:            clusterId,
 		NbProbes:             1,
 		XXX_NoUnkeyedLiteral: struct{}{},
 		XXX_unrecognized:     nil,
 		XXX_sizecache:        0,
 	})
-	if err != nil {
-		log.Fatalf("received error : %s", err)
-	}
-	//log.Printf("Client received: %s", r.Reports)
 
-	probes := r.Probes
+	if err != nil {
+		log.Errorf("received error : %s", err)
+		return nil, err
+	}
+
+	return reply, nil
+}
+
+
+func integerByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.2f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func analyse(clusterID string, timestamp string, nodes []*cockroachdb.NodeStats) *analysis {
+	a := &analysis{
+		clusterId:     clusterID,
+		timestamp:     timestamp,
+		nodes:         len(nodes),
+		usedCap:       "",
+		availableCap:  "",
+		queries:       0,
+		heartbeatP99:  "",
+		ranges:        0,
+		highestOffset: 0,
+		highestOffsetNodeId: 0,
+	}
+
+	var usedCap uint64 = 0
+	var availCap int64 = 0
+
+	for _, node := range nodes {
+		usedCap += node.CapacityUsage
+		availCap += int64(node.CapacityAvailable)
+		a.queries += node.QueriesPerSecond
+		if node.Ranges > a.ranges {
+			a.ranges = node.Ranges
+		}
+		if node.ClockOffset > a.highestOffset {
+			a.highestOffset = node.ClockOffset
+			a.highestOffsetNodeId = node.ID
+		}
+	}
+
+	// TODO P99
+
+	a.usedCap = fmt.Sprintf("%.2f%%", 100 * float64(usedCap) / float64(availCap))
+	a.availableCap = integerByteCountSI(availCap)
+
+	return a
+}
+
+func pullPrint(c *Connection) error {
+	// Contact the server
+	reply, err := c.Pull()
+	if err != nil {
+		return err
+	}
 
 	log.Infof("Client received a response !")
-	log.Infof("problen : %d", len(probes))
+
+	probes := reply.Probes
+	var nodes []*cockroachdb.NodeStats
+
 	for i, p := range probes {
 		log.Infof("probe %d : status %s", i, p.Status)
 		log.Infof("time  %s", p.Timestamp)
-		log.Infof("response len %d", p.Len)
-		log.Infof("payload (%d/%d): '%s'", len(p.Data[i]), len(string(p.Data[i])), string(p.Data[i]))
+
+		//node := &cockroachdb.NodeStats{}
+		err = json.Unmarshal(p.Data, &nodes)
+		if err != nil {
+			log.Error("Error in json : ", err)
+			continue
+		}
+
+		displayConsole(analyse(p.ClusterID, time.Now().Format("2019-08-16 23:43:04:4675"), nodes))
 	}
+
+	return nil
+}
+
+
+
+
+func main() {
+
+	// Set up a connection
+	connection := setupConnection()
+	defer connection.close()
+
+
+	// Sync
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	stop := make(chan struct{})
+
+
+	//time.Sleep(3 * time.Second)
+
+	ticker := time.NewTicker(refresh)
+
+
+
+	go func() {
+
+		for {
+			select {
+
+			case <-stop:
+				return
+
+			case <- ticker.C:
+				err := pullPrint(connection)
+				if err != nil {
+					sigs<-syscall.SIGINT
+				}
+			}
+		}
+	}()
+
+
+	// Intercept interruption
+
+	log.Info("Waiting for signal.")
+	s := <-sigs
+	log.Info("Signal received : ", s.String())
+
+	stop<- struct{}{}
+	close(stop)
+	ticker.Stop()
+
 	log.Infof("Client finished.")
 }
+
+/**
+*
+*
+*
+*
+*
+ */
+
+const (
+	clearConsole 	= "\x1Bc"
+	topLine			= green + "[dbmon]" + blue +" \tCluster :  %s" + stop + "\t\tLast updated : %s"
+	//noData			= "\t\t\t--- No available data ---"
+	summary			= "Health check summary :"
+	summaryNodes	= "Nodes : %d"
+	summaryUsedCap	= "Used Capacity : %s / %s"
+	summaryQueries	= "Queries per second : %.2f"
+	summaryP99		= "Heartbeat P99 Latency : %s ms"
+	database		= "Ranges : %d"
+	highestOffset	= "Highest Clock offset %d ns ( node %d )"
+
+
+	// ANSI Colours
+	red   = "\033[31;1;1m"
+	green = "\033[32m"
+	blue  = "\033[34m"
+	stop  = "\033[0m"
+)
+
+type analysis struct {
+	clusterId	string
+	timestamp	string // time.Now().Format("2019-08-17 23-03-18:745")
+	nodes		int
+	usedCap		string
+	availableCap string
+	queries		float64
+	heartbeatP99 string
+	ranges		uint
+	highestOffset	uint32
+	highestOffsetNodeId int
+}
+
+func displayConsole(a *analysis) {
+	var output string
+
+	output += fmt.Sprintf(topLine + "\n", a.clusterId, a.timestamp)
+	output += fmt.Sprint(summary) + "\n"
+	output += "\t\t> " + fmt.Sprintf(summaryNodes, a.nodes) + "\n"
+	output += "\t\t> " + fmt.Sprintf(summaryUsedCap, a.usedCap, a.availableCap) + "\n"
+	output += "\t\t> " + fmt.Sprintf(summaryQueries, a.queries) + "\n"
+	output += "\t\t> " + fmt.Sprintf(summaryP99, a.heartbeatP99) + "\n"
+	output += "\t\t> " + fmt.Sprintf(database, a.ranges) + "\n"
+	output += "\t\t> " + fmt.Sprintf(highestOffset, a.highestOffset, a.highestOffsetNodeId) + "\n"
+
+	fmt.Print(clearConsole)
+	fmt.Print(output)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- connectors/CockroachDB/connector.go
+++ connectors/CockroachDB/connector.go
@@ -8,14 +8,17 @@ TODO : Maybe using the cockroachdb command could help : but how would we do this
 */
 
 import (
+	"encoding/json"
 	"github.com/bytemare/dbmon/dbmon"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 const (
+	roachPeriod = 1 * time.Second
 	roachTimeout = 10 * time.Second
 )
 
@@ -46,12 +49,23 @@ func NewConnector(id string, ip string, port string) *RoachConnector {
 		clusterId: id,
 		ip:        ip,
 		port:      port,
-		period:    2 * time.Second,
+		period:    roachPeriod,
 		timeout:   roachTimeout,
 		netClient: &http.Client{
-			Timeout: time.Second * 10,
+			Timeout: roachTimeout,
 		},
 	}
+}
+
+// get operates a HTTP get request to the given endpoint, and returns the Response on success (else nil)
+func (rc RoachConnector) get(endpoint string) (*http.Response, error) {
+	resp, err := rc.netClient.Get(endpoint)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 /**
@@ -60,12 +74,14 @@ func NewConnector(id string, ip string, port string) *RoachConnector {
 **
  */
 
-// sendRequest sends a single request to the specified ressource
-func (rc RoachConnector) sendRequest(fullAdd string) (status string, result []byte, err error) {
-	resp, err := rc.netClient.Get(fullAdd)
+// getNodes returns a structure containing the response to a call to /_status/nodes
+func (rc RoachConnector) getNodes() ([]*NodeStats, error) {
+	resp, err := rc.get(rc.ip + ":" + rc.port + reqAllNodes)
 	if err != nil {
-		log.Error(err)
-		return resp.Status, nil, err
+		if resp != nil {
+			return nil, err
+		}
+		return nil, err
 	}
 
 	defer func() {
@@ -75,8 +91,121 @@ func (rc RoachConnector) sendRequest(fullAdd string) (status string, result []by
 		}
 	}()
 
-	log.Info("Response status: ", resp.Status)
+	nodes := &AllNodes{}
+	/*raw, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("Error in reading json : ", err)
+	} else {
+		//log.Infof("Read json successfully : %#v", raw)
+	}
+
+	*/
+	//err = json.Unmarshal(raw, &nodes)
+	err = json.NewDecoder(resp.Body).Decode(nodes)
+	if err != nil {
+		log.Error("getNodes : Error in parsing json : ", err)
+		return nil, err
+	}
+
+	nodeStats := make([]*NodeStats, 0, len(nodes.Nodes))
+
+	for _, n := range nodes.Nodes {
+		node := &NodeStats{
+			ID:                0,
+			Up:                false,
+			Init:              false,
+			Out:               false,
+			Dead:              false,
+			Ranges:            0,
+			CapacityUsage:     0,
+			CapacityReserved:  0,
+			CapacityAvailable: 0,
+			Capacity:          0,
+			TotalSystemMemory: 0,
+			MemoryUsage:       0,
+			MemoryReserved:    0,
+			MemoryAvailable:   0,
+			QueriesPerSecond:  0,
+			Latency99:         0,
+			ClockOffset:       0,
+		}
+		node.ID = n.Desc.NodeID
+		node.Ranges = uint(n.StoreStatuses[0].Desc.Capacity.RangeCount)
+
+		metrics := n.StoreStatuses[0].Metrics
+		node.Replicas = uint(metrics.Replicas)
+		node.CapacityUsage = uint64(metrics.CapacityUsed)
+		node.CapacityReserved = uint64(metrics.CapacityReserved)
+		node.CapacityAvailable = uint64(metrics.CapacityAvailable)
+		node.Capacity = uint64(metrics.Capacity)
+		node.TotalSystemMemory, _ = strconv.ParseInt(n.TotalSystemMemory, 10, 64)
+		node.QueriesPerSecond = n.StoreStatuses[0].Desc.Capacity.QueriesPerSecond
+		node.Latency99 = uint64(n.Metrics.LivenessHeartbeatlatencyP99)
+		node.ClockOffset = uint32(n.Metrics.ClockOffsetMeannanos)
+
+		nodeStats = append(nodeStats, node)
+		//log.Infof("Node Info : %#v", node)
+	}
+
+	return nodeStats, nil
+}
+
+func (rc RoachConnector) getRanges() (int, error) {
+	resp, err := rc.get(rc.ip + ":" + rc.port + reqClusterRaft)
+	if err != nil {
+		if resp != nil {
+			return 0, err
+		}
+		return 0, err
+	}
+
+	defer func() {
+		err = resp.Body.Close()
+		if err != nil {
+			log.Error("Closing response body failed. But who cares ?")
+		}
+	}()
+
+	raft := &ClusterRaft{}
+	err = json.NewDecoder(resp.Body).Decode(raft)
+	if err != nil {
+		log.Error("getRanges : Error in parsing json : ", err)
+	} else {
+		//log.Infof("getNodes : Parsed json successfully : %#v", nodes)
+	}
+	//log.Infof("Length of nodes : %d", len(ranges))
+
+	ranges := len(raft.Ranges)
+	log.Infof("getRanges : counted %d ranges", ranges)
+
+	return ranges, nil
+}
+
+// getHealth sends a single request to the specified resource
+func (rc RoachConnector) getHealth(fullAdd string) (status string, result []byte, err error) {
+	resp, err := rc.get(fullAdd)
+	if err != nil {
+		if resp != nil {
+			return resp.Status, nil, err
+		}
+		return "", nil, err
+	}
+
+	//log.Info("Response status: ", resp.Status)
 	raw, _ := ioutil.ReadAll(resp.Body)
+
+	//var h Health
+	var test1 map[string]interface{}
+
+	err = json.Unmarshal(raw, &test1)
+	if err != nil {
+		//log.Error("Error in parsing json : ", err)
+	}
+	//log.Infof("Parsed json successfully : %#v", test1)
+
+	//test2 := dyno.ConvertMapI2MapS(test1)
+	//log.Infof("Remapped json : %#v", test2)
+
 	//log.Info("Raw response ", raw)
 	//log.Info("String response ", string(raw))
 	//return resp.Status, string(raw), nil
@@ -87,17 +216,28 @@ func (rc RoachConnector) sendRequest(fullAdd string) (status string, result []by
 func (rc RoachConnector) Probe() (probe *dbmon.Probe, err error) {
 
 	log.Info("Sending request to Cluster ", rc.clusterId)
-	status, res, err := rc.sendRequest(rc.ip + ":" + rc.port + reqHealth)
+	/*status, res, err := rc.getHealth(rc.ip + ":" + rc.port + reqHealth)
+	if err != nil {
+		return nil, err
+	}
+
+	*/
+
+	// Get Number of nodes and their ID
+	nodeStats, err := rc.getNodes()
+	if err != nil {
+		return nil, err
+	}
+	payload, err := json.Marshal(nodeStats)
 	if err != nil {
 		return nil, err
 	}
 
 	probe = &dbmon.Probe{
 		ClusterID:            rc.clusterId,
-		Status:               status,
+		Status:               "200",
 		Timestamp:            time.Now().String(),
-		Len:                  []int32{int32(len(res))},
-		Data:                 [][]byte{res},
+		Data:                 payload,
 		XXX_NoUnkeyedLiteral: struct{}{},
 		XXX_unrecognized:     nil,
 		XXX_sizecache:        0,
