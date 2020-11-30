6b05aa5e08b1622468fc9871791b8f6c97490298
bytemare <{ID}+{username}@users.noreply.github.com>
2020-11-30 20:28:44 +0100

    safe/backup

--- connector.go
+++ connector.go
@@ -8,6 +8,17 @@ import (
 // Connector is a request abstraction layer to the dbmon collector.
 // It will adapt generic queries to the specified database endpoint.
 type Connector interface {
+/*
+	// Metadata about a connector for the collector
+	Id() (id string)
+	Ip() (ip string)
+	Port() (port string)
+	Status() (status string) // Responsive/Online, Pending, Unauthorised, Unreachable, Stopped/Offline
+	Stop() // Tells the connector to stop and free resources
+	// TODO : maybe use closures to repetitively work on same object ?
+
+
+*/
 	// Probe asks the connector to operate a probe to the cluster
 	Probe() (probe *Probe, err error)
 
